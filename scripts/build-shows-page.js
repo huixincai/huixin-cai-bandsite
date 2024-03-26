20a9b36c158c28142d260f7d1e7c03ccd979d445
Huixin Cai <caihuixin2009@hotmail.com>
2024-03-25 20:06:24 -0400

    format files

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -4,198 +4,205 @@ const showsTable = document.querySelector(".shows__table");
 const showsTableBody = document.querySelector(".shows__table-body");
 
 const shows = [
-    {
-        date: "Mon Sept 09 2024",
-        venue: "Ronald Lane",
-        location: "San Francisco, CA"
-    }, {
-        date: "Tue Sept 17 2024",
-        venue: "Pier 3 East",
-        location: "San Francisco, CA"
-    }, {
-        date: "Sat Oct 12 2024",
-        venue: "View Lounge",
-        location: "San Francisco, CA"
-    }, {
-        date: "Sat Nov 16 2024",
-        venue: "Hyatt Agency",
-        location: "San Francisco, CA"
-    }, {
-        date: "Fri Nov 29 2024",
-        venue: "Moscow Center",
-        location: "San Francisco, CA"
-    }, {
-        date: "Wed Dec 18 2024",
-        venue: "Press Club",
-        location: "San Francisco, CA"
-    }
+  {
+    date: "Mon Sept 09 2024",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Tue Sept 17 2024",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Sat Oct 12 2024",
+    venue: "View Lounge",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Sat Nov 16 2024",
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Nov 29 2024",
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Wed Dec 18 2024",
+    venue: "Press Club",
+    location: "San Francisco, CA",
+  },
 ];
 
 function addTableRowSelectListener() {
-    const tableDataRows = document.querySelectorAll(".shows__table-data-row");
+  const tableDataRows = document.querySelectorAll(".shows__table-data-row");
 
-    tableDataRows.forEach(element => {
-        element.addEventListener("click", (e) => {
-            e.preventDefault();
-            if (e.target.tagName.toLowerCase() === "td") {
-                const tableDataRow = e.target.parentElement;
-    
-                // remove existing active rows
-                const activeRows = document.querySelectorAll(".shows__table-data-row--active");
-                activeRows.forEach(rowEl => {
-                    rowEl.classList.remove("shows__table-data-row--active");
-                });
-    
-                tableDataRow.classList.add("shows__table-data-row--active");
-            }
-        })
+  tableDataRows.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (e.target.tagName.toLowerCase() === "td") {
+        const tableDataRow = e.target.parentElement;
+
+        // remove existing active rows
+        const activeRows = document.querySelectorAll(
+          ".shows__table-data-row--active"
+        );
+        activeRows.forEach((rowEl) => {
+          rowEl.classList.remove("shows__table-data-row--active");
+        });
+
+        tableDataRow.classList.add("shows__table-data-row--active");
+      }
     });
+  });
 }
 
 function addListItemSelectListener() {
-    const listItems = document.querySelectorAll(".shows__item");
+  const listItems = document.querySelectorAll(".shows__item");
 
-    listItems.forEach(element => {
-        element.addEventListener("click", e => {
-            e.preventDefault();
-            let listItemEl = e.target;
-            if (listItemEl.parentElement.classList.contains("shows__item-field")) {
-                listItemEl = listItemEl.parentElement;
-            }
-            if (listItemEl.classList.contains("shows__item-field")) {
-                listItemEl = listItemEl.parentElement;
-            }
-            if (listItemEl.classList.contains("shows__item")) {
-                // remove existing active items
-                const activeItems = document.querySelectorAll(".shows__item--active");
-                activeItems.forEach(itemEl => {
-                    itemEl.classList.remove("shows__item--active");
-                });
-                listItemEl.classList.add("shows__item--active");
-            }
-        })
+  listItems.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      e.preventDefault();
+      let listItemEl = e.target;
+      if (listItemEl.parentElement.classList.contains("shows__item-field")) {
+        listItemEl = listItemEl.parentElement;
+      }
+      if (listItemEl.classList.contains("shows__item-field")) {
+        listItemEl = listItemEl.parentElement;
+      }
+      if (listItemEl.classList.contains("shows__item")) {
+        // remove existing active items
+        const activeItems = document.querySelectorAll(".shows__item--active");
+        activeItems.forEach((itemEl) => {
+          itemEl.classList.remove("shows__item--active");
+        });
+        listItemEl.classList.add("shows__item--active");
+      }
     });
+  });
 }
 
 function addShowsTableRow(show) {
-    const tableRowEl = document.createElement("tr");
-    tableRowEl.classList.add("shows__table-data-row");
+  const tableRowEl = document.createElement("tr");
+  tableRowEl.classList.add("shows__table-data-row");
 
-    const showDateEl = document.createElement("td");
-    showDateEl.classList.add("shows__table-data-date");
-    showDateEl.innerText = show.date;
+  const showDateEl = document.createElement("td");
+  showDateEl.classList.add("shows__table-data-date");
+  showDateEl.innerText = show.date;
 
-    const showVenueEl = document.createElement("td");
-    showVenueEl.innerText = show.venue;
+  const showVenueEl = document.createElement("td");
+  showVenueEl.innerText = show.venue;
 
-    const showLocationEl = document.createElement("td");
-    showLocationEl.innerText = show.location;
+  const showLocationEl = document.createElement("td");
+  showLocationEl.innerText = show.location;
 
-    const showBuyCellEl = document.createElement("td");
-    showBuyCellEl.classList.add("shows__table-data-button");
+  const showBuyCellEl = document.createElement("td");
+  showBuyCellEl.classList.add("shows__table-data-button");
 
-    const showButtonEl = document.createElement("button");
-    showButtonEl.innerText = "BUY TICKETS";
+  const showButtonEl = document.createElement("button");
+  showButtonEl.innerText = "BUY TICKETS";
 
-    showBuyCellEl.appendChild(showButtonEl);
+  showBuyCellEl.appendChild(showButtonEl);
 
-    tableRowEl.appendChild(showDateEl);
-    tableRowEl.appendChild(showVenueEl);
-    tableRowEl.appendChild(showLocationEl);
-    tableRowEl.appendChild(showBuyCellEl);
+  tableRowEl.appendChild(showDateEl);
+  tableRowEl.appendChild(showVenueEl);
+  tableRowEl.appendChild(showLocationEl);
+  tableRowEl.appendChild(showBuyCellEl);
 
-    showsTableBody.appendChild(tableRowEl);
+  showsTableBody.appendChild(tableRowEl);
 }
 
 function addShowsListItem(show) {
-    const listItemEl = document.createElement("div");
-    listItemEl.classList.add("shows__item");
+  const listItemEl = document.createElement("div");
+  listItemEl.classList.add("shows__item");
 
-    const listItemDateFieldEl = document.createElement("div");
-    listItemDateFieldEl.classList.add("shows__item-field");
+  const listItemDateFieldEl = document.createElement("div");
+  listItemDateFieldEl.classList.add("shows__item-field");
 
-    const listItemDateLabel = document.createElement("div");
-    listItemDateLabel.classList.add("shows__item-label");
-    listItemDateLabel.innerText = "DATE";
+  const listItemDateLabel = document.createElement("div");
+  listItemDateLabel.classList.add("shows__item-label");
+  listItemDateLabel.innerText = "DATE";
 
-    const listItemDateValue = document.createElement("div");
-    listItemDateValue.classList.add("shows__item-value");
-    listItemDateValue.classList.add("shows__item-date");
-    listItemDateValue.innerText = show.date;
+  const listItemDateValue = document.createElement("div");
+  listItemDateValue.classList.add("shows__item-value");
+  listItemDateValue.classList.add("shows__item-date");
+  listItemDateValue.innerText = show.date;
 
-    listItemDateFieldEl.appendChild(listItemDateLabel);
-    listItemDateFieldEl.appendChild(listItemDateValue);
+  listItemDateFieldEl.appendChild(listItemDateLabel);
+  listItemDateFieldEl.appendChild(listItemDateValue);
 
-    const listItemVenueFieldEl = document.createElement("div");
-    listItemVenueFieldEl.classList.add("shows__item-field");
+  const listItemVenueFieldEl = document.createElement("div");
+  listItemVenueFieldEl.classList.add("shows__item-field");
 
-    const listItemVenueLabel = document.createElement("div");
-    listItemVenueLabel.classList.add("shows__item-label");
-    listItemVenueLabel.innerText = "VENUE";
+  const listItemVenueLabel = document.createElement("div");
+  listItemVenueLabel.classList.add("shows__item-label");
+  listItemVenueLabel.innerText = "VENUE";
 
-    const listItemVenueValue = document.createElement("div");
-    listItemVenueValue.classList.add("shows__item-value");
-    listItemVenueValue.innerText = show.venue;
+  const listItemVenueValue = document.createElement("div");
+  listItemVenueValue.classList.add("shows__item-value");
+  listItemVenueValue.innerText = show.venue;
 
-    listItemVenueFieldEl.appendChild(listItemVenueLabel);
-    listItemVenueFieldEl.appendChild(listItemVenueValue);
+  listItemVenueFieldEl.appendChild(listItemVenueLabel);
+  listItemVenueFieldEl.appendChild(listItemVenueValue);
 
-    const listItemLocationFieldEl = document.createElement("div");
-    listItemLocationFieldEl.classList.add("shows__item-field");
+  const listItemLocationFieldEl = document.createElement("div");
+  listItemLocationFieldEl.classList.add("shows__item-field");
 
-    const listItemLocationLabel = document.createElement("div");
-    listItemLocationLabel.classList.add("shows__item-label");
-    listItemLocationLabel.innerText = "LOCATION";
+  const listItemLocationLabel = document.createElement("div");
+  listItemLocationLabel.classList.add("shows__item-label");
+  listItemLocationLabel.innerText = "LOCATION";
 
-    const listItemLocationValue = document.createElement("div");
-    listItemLocationValue.classList.add("shows__item-value");
-    listItemLocationValue.innerText = show.location;
+  const listItemLocationValue = document.createElement("div");
+  listItemLocationValue.classList.add("shows__item-value");
+  listItemLocationValue.innerText = show.location;
 
-    listItemLocationFieldEl.appendChild(listItemLocationLabel);
-    listItemLocationFieldEl.appendChild(listItemLocationValue);
+  listItemLocationFieldEl.appendChild(listItemLocationLabel);
+  listItemLocationFieldEl.appendChild(listItemLocationValue);
 
-    const listItemTicketFieldEl = document.createElement("div");
-    listItemTicketFieldEl.classList.add("shows__item-field");
+  const listItemTicketFieldEl = document.createElement("div");
+  listItemTicketFieldEl.classList.add("shows__item-field");
 
-    const listItemTicketButton = document.createElement("button");
-    listItemTicketButton.classList.add("shows__item-button");
-    listItemTicketButton.innerText = "BUY TICKETS";
+  const listItemTicketButton = document.createElement("button");
+  listItemTicketButton.classList.add("shows__item-button");
+  listItemTicketButton.innerText = "BUY TICKETS";
 
-    listItemTicketFieldEl.appendChild(listItemTicketButton);
+  listItemTicketFieldEl.appendChild(listItemTicketButton);
 
-    listItemEl.appendChild(listItemDateFieldEl);
-    listItemEl.appendChild(listItemVenueFieldEl);
-    listItemEl.appendChild(listItemLocationFieldEl);
-    listItemEl.appendChild(listItemTicketFieldEl);
+  listItemEl.appendChild(listItemDateFieldEl);
+  listItemEl.appendChild(listItemVenueFieldEl);
+  listItemEl.appendChild(listItemLocationFieldEl);
+  listItemEl.appendChild(listItemTicketFieldEl);
 
-    showsList.appendChild(listItemEl);
+  showsList.appendChild(listItemEl);
 
-    const divider = document.createElement("div");
-    divider.classList.add("divider");
-    showsList.appendChild(divider);
+  const divider = document.createElement("div");
+  divider.classList.add("divider");
+  showsList.appendChild(divider);
 }
 
 function renderShows() {
-    if (window.innerWidth >= 768) {
-        showsTable.classList.remove("shows__table--hide");
-        shows.forEach(addShowsTableRow);
-        addTableRowSelectListener();
-    } else {
-        showsTable.classList.add("shows__table--hide");
-        shows.forEach(addShowsListItem);
-        addListItemSelectListener();
-    }
+  if (window.innerWidth >= 768) {
+    showsTable.classList.remove("shows__table--hide");
+    shows.forEach(addShowsTableRow);
+    addTableRowSelectListener();
+  } else {
+    showsTable.classList.add("shows__table--hide");
+    shows.forEach(addShowsListItem);
+    addListItemSelectListener();
+  }
 }
 
-window.addEventListener("resize", function() {
-    while (showsTableBody.firstChild) {
-        showsTableBody.removeChild(showsTableBody.firstChild);
-    }
-    while (showsList.firstChild) {
-        showsList.removeChild(showsList.firstChild);
-    }
+window.addEventListener("resize", function () {
+  while (showsTableBody.firstChild) {
+    showsTableBody.removeChild(showsTableBody.firstChild);
+  }
+  while (showsList.firstChild) {
+    showsList.removeChild(showsList.firstChild);
+  }
 
-    renderShows();
+  renderShows();
 });
 
 renderShows();
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,95 +1,107 @@
 // Select the form element
-const commentForm = document.querySelector('.comments__comment-form');
+const commentForm = document.querySelector(".comments__comment-form");
 
 // Select the appointment list element
-const commentListElement = document.querySelector('.comments__comments-list');
+const commentListElement = document.querySelector(".comments__comments-list");
 
 const comments = [
-    { name: "Victor Pinto", timestamp: "11/02/2023", text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains." },
-    { name: "Christina Cabrera", timestamp: "10/28/2023", text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day." },
-    { name: "Isaac Tadesse", timestamp: "10/20/2023", text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough." }
+  {
+    name: "Victor Pinto",
+    timestamp: "11/02/2023",
+    text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    name: "Christina Cabrera",
+    timestamp: "10/28/2023",
+    text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Isaac Tadesse",
+    timestamp: "10/20/2023",
+    text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
 ];
 
 commentForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const name = e.target.nameId.value;
-    const text = e.target.commentId.value;
+  const name = e.target.nameId.value;
+  const text = e.target.commentId.value;
 
-    // this generates a Unix timestamp
-    const currTimestamp = Date.now();
-    const currDate = new Date(currTimestamp);
+  // this generates a Unix timestamp
+  const currTimestamp = Date.now();
+  const currDate = new Date(currTimestamp);
 
-    // mm/dd/yyyy
-    const month = currDate.getMonth();
-    const date = currDate.getDate();
-    const year = currDate.getFullYear();
+  // mm/dd/yyyy
+  const month = currDate.getMonth();
+  const date = currDate.getDate();
+  const year = currDate.getFullYear();
 
-    const timestamp = `${month}/${date}/${year}`;
+  const timestamp = `${month}/${date}/${year}`;
 
-    const newComment = {
-        name,
-        text,
-        timestamp,
-    }
+  const newComment = {
+    name,
+    text,
+    timestamp,
+  };
 
-    comments.unshift(newComment);
-    renderCommentsList();
+  comments.unshift(newComment);
+  renderCommentsList();
 });
 
 function createComment(commentObj) {
-    // create element for the comment item
-    const commentItemEl = document.createElement("div");
-    commentItemEl.classList.add("comments__comment-item");
+  // create element for the comment item
+  const commentItemEl = document.createElement("div");
+  commentItemEl.classList.add("comments__comment-item");
 
-    const commentAvatarEl = document.createElement("div");
-    commentAvatarEl.classList.add("comments__comment-avatar");
+  const commentAvatarEl = document.createElement("div");
+  commentAvatarEl.classList.add("comments__comment-avatar");
 
-    const commentSectionEl = document.createElement("div");
-    commentSectionEl.classList.add("comments__comment-section");
+  const commentSectionEl = document.createElement("div");
+  commentSectionEl.classList.add("comments__comment-section");
 
-    commentItemEl.appendChild(commentAvatarEl);
-    commentItemEl.appendChild(commentSectionEl);
+  commentItemEl.appendChild(commentAvatarEl);
+  commentItemEl.appendChild(commentSectionEl);
 
-    const commentHeaderEl = document.createElement("div");
-    commentHeaderEl.classList.add("comments__comment-header");
+  const commentHeaderEl = document.createElement("div");
+  commentHeaderEl.classList.add("comments__comment-header");
 
-    const commentContentEl = document.createElement("div");
-    commentContentEl.classList.add("comments__comment-content");
-    commentContentEl.innerText = commentObj.text;
+  const commentContentEl = document.createElement("div");
+  commentContentEl.classList.add("comments__comment-content");
+  commentContentEl.innerText = commentObj.text;
 
-    commentSectionEl.appendChild(commentHeaderEl);
-    commentSectionEl.appendChild(commentContentEl);
+  commentSectionEl.appendChild(commentHeaderEl);
+  commentSectionEl.appendChild(commentContentEl);
 
-    const commentHeaderNameEl = document.createElement("h3");
-    commentHeaderNameEl.classList.add("comments__comment-header-name");
-    commentHeaderNameEl.innerText = commentObj.name;
+  const commentHeaderNameEl = document.createElement("h3");
+  commentHeaderNameEl.classList.add("comments__comment-header-name");
+  commentHeaderNameEl.innerText = commentObj.name;
 
-    const commentHeaderTimestampEl = document.createElement("div");
-    commentHeaderTimestampEl.classList.add("comments__comment-header-timestamp");
-    commentHeaderTimestampEl.innerText = commentObj.timestamp;
+  const commentHeaderTimestampEl = document.createElement("div");
+  commentHeaderTimestampEl.classList.add("comments__comment-header-timestamp");
+  commentHeaderTimestampEl.innerText = commentObj.timestamp;
 
-    commentHeaderEl.appendChild(commentHeaderNameEl);
-    commentHeaderEl.appendChild(commentHeaderTimestampEl);
+  commentHeaderEl.appendChild(commentHeaderNameEl);
+  commentHeaderEl.appendChild(commentHeaderTimestampEl);
 
-    commentListElement.appendChild(commentItemEl);
+  commentListElement.appendChild(commentItemEl);
 }
 
 function createDivider() {
-    const divider = document.createElement("div");
-    divider.classList.add("divider");
-    commentListElement.appendChild(divider);
+  const divider = document.createElement("div");
+  divider.classList.add("divider");
+  commentListElement.appendChild(divider);
 }
 
 function renderCommentsList() {
-    while (commentListElement.firstChild) {
-        commentListElement.removeChild(commentListElement.firstChild);
-    }
+  while (commentListElement.firstChild) {
+    commentListElement.removeChild(commentListElement.firstChild);
+  }
+  createDivider();
+  comments.forEach((comment) => {
+    createComment(comment);
     createDivider();
-    comments.forEach(comment => {
-        createComment(comment);
-        createDivider();
-    });
+  });
 }
 
 renderCommentsList();
